14677a56cb2ebc38f19cb850f4578aab5ef5e28b
AndersonMamede <mamede.anderson@gmail.com>
2019-01-28 16:40:52 -0200

    Add TotalHitsCounter

--- app/src/components/HomePage.js
+++ app/src/components/HomePage.js
@@ -4,17 +4,19 @@ import PropTypes from 'prop-types';
 import { shortenedItemDataType } from '../types';
 import FormUrlShortener from './FormUrlShortener';
 import TopFive from './TopFive';
+import TotalHitsCounter from './TotalHitsCounter';
 
-const HomePage = ({ topFive }) => (
-  <main>
+const HomePage = ({ topFive, totalHits }) => (
+  <React.Fragment>
     <FormUrlShortener />
     <TopFive itens={topFive} />
-    {/* <TopHits /> */}
-  </main>
+    <TotalHitsCounter totalHits={totalHits}/>
+  </React.Fragment>
 );  
 
 HomePage.propTypes = {
-  topFive: PropTypes.arrayOf(shortenedItemDataType)
+  topFive: PropTypes.arrayOf(shortenedItemDataType),
+  totalHits: PropTypes.number.isRequired,
 };
 
 export default HomePage;

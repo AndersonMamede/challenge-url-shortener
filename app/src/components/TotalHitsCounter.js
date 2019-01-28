30ad3e2ed9e8f347a3b7d58742352af218d73f21
AndersonMamede <mamede.anderson@gmail.com>
2019-01-28 17:40:51 -0200

    Style adjusts

--- app/src/components/TotalHitsCounter.js
+++ app/src/components/TotalHitsCounter.js
@@ -3,13 +3,13 @@ import React from 'react';
 import styles from './TotalHitsCounter.module.css';
 
 const TotalHitsCounter = ({ totalHits }) => (
-  <section className="section gray_bg text_center">
+  <section className="section section_white_triangle gray_bg text_center">
     <div className="section_container">
       <h1 className="section_title">Hits</h1>
       <div>
         <span className={styles.counter}>{Number(totalHits).toLocaleString('pt-br')}</span>
       </div>
-      <div className={styles.observation}>Cliques em links</div>
+      <div className={styles.observation}>Clicks on links</div>
     </div>
   </section>
 );

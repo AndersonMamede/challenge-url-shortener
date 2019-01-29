942458f230dfcffef484f20256f548130d0a06a0
AndersonMamede <mamede.anderson@gmail.com>
2019-01-29 09:45:52 -0200

    Build

---
+++ app/build/precache-manifest.f1198feac69d08561b8760488e295e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7563a5f71626df8dd20c88ad4321c86",
+    "url": "/challenge-url-shortener/app/build/static/media/favicon.d7563a5f.ico"
+  },
+  {
+    "revision": "102b62285014ff022ac4",
+    "url": "/challenge-url-shortener/app/build/static/js/runtime~main.102b6228.js"
+  },
+  {
+    "revision": "63c1d2222f7e95e7d5eb",
+    "url": "/challenge-url-shortener/app/build/static/js/main.63c1d222.chunk.js"
+  },
+  {
+    "revision": "9d7093c6f8c22c5dbc45",
+    "url": "/challenge-url-shortener/app/build/static/js/1.9d7093c6.chunk.js"
+  },
+  {
+    "revision": "63c1d2222f7e95e7d5eb",
+    "url": "/challenge-url-shortener/app/build/static/css/main.1840d90a.chunk.css"
+  },
+  {
+    "revision": "f983c8da50e1431932e989433257c1b9",
+    "url": "/challenge-url-shortener/app/build/index.html"
+  }
+];

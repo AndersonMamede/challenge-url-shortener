f472523604a23a03f7515e07986cfa2011f03b35
AndersonMamede <mamede.anderson@gmail.com>
2019-01-31 17:45:33 -0200

    Add bg and color props

--- app/src/components/ProjectBadge.js
+++ app/src/components/ProjectBadge.js
@@ -3,19 +3,26 @@ import PropTypes from 'prop-types';
 
 import styles from './ProjectBadge.module.css';
 
-const ProjectBadge = ({ url, text }) => (
-  <a className={styles.badge} href={url} target="_blank" rel="noopener noreferrer">
-    {text}
-  </a>
+const ProjectBadge = ({ url, text, background, color }) => (
+  <a
+    className={styles.badge}
+    href={url}
+    style={{ background, color }}
+    target="_blank" rel="noopener noreferrer"
+  >{text}</a>
 );
 
 ProjectBadge.propTypes = {
   url: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  background: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
 };
 
 ProjectBadge.defaultProps = {
   text: 'About this project',
+  background: '#1390F3',
+  color: '#FFF',
 }
 
 export default ProjectBadge;
